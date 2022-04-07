2e935da2114b155ee146fb90b7d56b439afd2fd8
Chris <christophercalloway216@gmail.com>
2022-04-07 01:03:00 -0700

    fixed folder name

--- runtests.py
+++ runtests.py
@@ -93,59 +93,59 @@ def test_conv_gold_tiled_test():
 
 def test_mac_tb():
     print("Running test_mac_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/mac_tb.v', 'verilog/mac.v'])          
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/mac_tb.v', 'rtl/mac.v'])          
 
 def test_mac_tb_uvm():
     print("Running test_mac_tb_uvm")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'cpp/mac_gold.c', 'tests/mac_tb_uvm.v', 'verilog/mac.v'])          
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'cpp/mac_gold.c', 'tests/mac_tb_uvm.v', 'rtl/mac.v'])          
 
 def test_skew_registers_tb():
     print("Running test_skew_registers_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/skew_registers_tb.v', 'verilog/skew_registers.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/skew_registers_tb.v', 'rtl/skew_registers.v'])
 
 def test_fifo_tb_uvm():
     print("Running test_fifo_tb_uvm")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/fifo_tb_uvm.v', 'verilog/fifo.v', 'verilog/SizedFIFO.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/fifo_tb_uvm.v', 'rtl/fifo.v', 'rtl/SizedFIFO.v'])
 
 def test_fifo_tb():
     print("Running test_fifo_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/fifo_tb.v', 'verilog/fifo.v', 'verilog/SizedFIFO.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/fifo_tb.v', 'rtl/fifo.v', 'rtl/SizedFIFO.v'])
 
 def test_ram_sync_1r1w_tb_uvm():
     print("Running test_ram_sync_1r1w_tb_uvm")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/ram_sync_1r1w_tb_uvm.v', 'verilog/ram_sync_1r1w.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/ram_sync_1r1w_tb_uvm.v', 'rtl/ram_sync_1r1w.v'])
 
 def test_aggregator_tb():
     print("Running test_aggregator_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/aggregator_tb.v', 'verilog/aggregator.v', 'verilog/fifo.v', 'verilog/SizedFIFO.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/aggregator_tb.v', 'rtl/aggregator.v', 'rtl/fifo.v', 'rtl/SizedFIFO.v'])
 
 def test_adr_gen_sequential_tb():
     print("Running test_adr_gen_sequential_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/adr_gen_sequential_tb.v', 'verilog/adr_gen_sequential.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/adr_gen_sequential_tb.v', 'rtl/adr_gen_sequential.v'])
 
 def test_adr_gen_sequential_tb_uvm():
     print("Running test_adr_gen_sequential_tb_uvm")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/adr_gen_sequential_tb_uvm.v', 'verilog/adr_gen_sequential.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/adr_gen_sequential_tb_uvm.v', 'rtl/adr_gen_sequential.v'])
 
 def test_ifmap_radr_gen_tb():
     print("Running test_ifmap_radr_gen_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/ifmap_radr_gen_tb.v', 'verilog/ifmap_radr_gen.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/ifmap_radr_gen_tb.v', 'rtl/ifmap_radr_gen.v'])
 
 def test_systolic_array_with_skew_tb():
     print("Running test_systolic_array_with_skew_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/systolic_array_with_skew_tb.v', 'verilog/systolic_array_with_skew.v', 'verilog/skew_registers.v', 'verilog/systolic_array.v', 'verilog/mac.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/systolic_array_with_skew_tb.v', 'rtl/systolic_array_with_skew.v', 'rtl/skew_registers.v', 'rtl/systolic_array.v', 'rtl/mac.v'])
 
 def test_deaggregator_tb():
     print("Running test_deaggregator_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/deaggregator_tb.v', 'verilog/deaggregator.v', 'verilog/fifo.v', 'verilog/SizedFIFO.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/deaggregator_tb.v', 'rtl/deaggregator.v', 'rtl/fifo.v', 'rtl/SizedFIFO.v'])
 
 def test_double_buffer_tb():
     print("Running test_double_buffer_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/double_buffer_tb.v', 'verilog/double_buffer.v', 'verilog/ram_sync_1r1w.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/double_buffer_tb.v', 'rtl/double_buffer.v', 'rtl/ram_sync_1r1w.v'])
 
 def test_accumulation_buffer_tb():
     print("Running test_accumulation_buffer_tb")
-    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/accumulation_buffer_tb.v', 'verilog/accumulation_buffer.v', 'verilog/ram_sync_1r1w.v'])
+    return 1, run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/accumulation_buffer_tb.v', 'rtl/accumulation_buffer.v', 'rtl/ram_sync_1r1w.v'])
 
 
 def test_conv_tb():
@@ -200,7 +200,7 @@ const int STRIDE = {data["STRIDE"]};
             print(process.stdout)
 
         run += 1
-        passed += run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/conv_tb.v', 'verilog/conv.v', 'verilog/systolic_array_with_skew.v', 'verilog/skew_registers.v', 'verilog/systolic_array.v', 'verilog/mac.v', 'verilog/adr_gen_sequential.v', 'verilog/deaggregator.v', 'verilog/aggregator.v', 'verilog/fifo.v', 'verilog/SizedFIFO.v', 'verilog/conv_controller.v', 'verilog/accumulation_buffer.v', 'verilog/ram_sync_1r1w.v', 'verilog/ifmap_radr_gen.v', 'verilog/double_buffer.v'])
+        passed += run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/conv_tb.v', 'rtl/conv.v', 'rtl/systolic_array_with_skew.v', 'rtl/skew_registers.v', 'rtl/systolic_array.v', 'rtl/mac.v', 'rtl/adr_gen_sequential.v', 'rtl/deaggregator.v', 'rtl/aggregator.v', 'rtl/fifo.v', 'rtl/SizedFIFO.v', 'rtl/conv_controller.v', 'rtl/accumulation_buffer.v', 'rtl/ram_sync_1r1w.v', 'rtl/ifmap_radr_gen.v', 'rtl/double_buffer.v'])
     return run, passed
 
 

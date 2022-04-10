4265ea74c5523440f3ce69791e67e3e2a8c33273
Chris <christophercalloway216@gmail.com>
2022-04-10 11:52:09 -0700

    added internal node rtl + tb

---
+++ rtl/internal_node.v
@@ -0,0 +1,89 @@
+/*
+ A module for an internal node of a KD-Tree
+ A set of these nodes will be instantiated together to make an actual tree,
+ this is a physical description of the node of the tree. 
+
+  Author: Chris Calloway, cmc2374@stanford.edu
+*/
+
+
+module internal_node
+#(
+  parameter DATA_WIDTH = 55,
+  parameter STORAGE_WIDTH = 22
+)
+(
+  input clk,
+  input rst_n,
+  input wen, //Determined by FSM, reciever enq, and DECODER from KD Tree
+  input valid,
+  input [STORAGE_WIDTH -1 : 0] wdata,
+  input [DATA_WIDTH - 1 : 0] patch_in,
+  output [DATA_WIDTH - 1 : 0] patch_out, //Same patch, but we will be pipeling so it will be useful to adopt this input/ouput scheme
+  output valid_left,
+  output valid_right
+
+);
+
+
+reg [2:0] idx;
+reg signed [10: 0] median; 
+reg signed [10: 0] sliced_patch;
+
+wire median_comparison;
+
+//Wdata: 1st 11 bits is Index (which can slice to the  3 LSB bits) since we gave 5 indeces, and 5 < 2^3.
+// 2nd 11 bits are the Median, for which we must store the entire 11 bits
+
+//IDX Storage
+always @ (clk) begin
+
+    if (rst_n == 0) begin
+        idx <= 3'b111; //-1 is an invalid index, this by default we know this to be untrue
+    end
+    else if (wen) begin
+        idx <= idx[2:0]; //Get 3 LSB
+    end
+    else begin
+        idx <= idx; //No change / persist in memory 
+    end
+
+end
+
+
+//Median Storage
+always @ (clk) begin
+
+    if (rst_n == 0) begin
+        median <= 0; //0 is an urealistic median, this by default we (likely) know this to be untrue. The -1 idx is the true debug test
+    end
+    else if (wen) begin
+        median <= wdata[21:11]; //Get Median
+    end
+    else begin
+        median <= median; //No change / persist in memory 
+    end
+
+end
+
+//Slice Component to get the proper value from the incoming patch based on stored dimension.
+always @(*) begin 
+    case(idx)
+       3'b000 :   sliced_patch = patch[10:0];
+       3'b001 :   sliced_patch = patch[21:11];
+       3'b010 :   sliced_patch = patch[32:22];
+       3'b011 :   sliced_patch = patch[43:33];
+       3'b100 :   sliced_patch = patch[55:45];
+       default :  sliced_patch = 11'b0;
+    endcase 
+end
+
+
+assign comparison = (sliced_patch < median);
+
+assign valid_left = comparison && valid;
+assign valid_right = (!comparison) && valid;
+
+
+
+endmodule
---
+++ tests/internal_node_tb.v
@@ -0,0 +1,89 @@
+`define DATA_WIDTH 55
+`define STORAGE_WIDTH 22
+
+
+module internal_node_tb;
+
+  reg clk;
+  reg rst_n;
+  reg wen;
+  reg valid;
+
+  reg [`STORAGE_WIDTH -1 : 0] wdata;
+  reg [`DATA_WIDTH - 1 : 0] patch_in;
+  wire [`DATA_WIDTH - 1 : 0] patch_out; //Same patch, but we will be pipeling so it will be useful to adopt this input/ouput scheme
+  wire valid_left;
+  wire valid_right;
+
+
+  always #10 clk =~clk;
+
+
+  internal_node
+  #(
+   .DATA_WIDTH(`DATA_WIDTH),
+   .STORAGE_WIDTH(`STORAGE_WIDTH)
+  )
+  dut
+  (
+  .clk(clk),
+  .rst_n(rst_n),
+  .wen(wen), //Determined by FSM, reciever enq, and DECODER from KD Tree
+  .valid(valid),
+  .wdata(wdata),
+  .patch_in(patch_in),
+  .patch_out(patch_out), //Same patch, but we will be pipeling so it will be useful to adopt this input/ouput scheme
+  .valid_left(valid_left),
+  .valid_right(valid_right)
+  );
+
+
+
+   initial begin
+    clk <= 0;
+    rst_n <= 0;
+    wen <= 0;
+    valid <= 0;
+    wdata <= 0;
+    patch_in <= 0;
+   
+
+    #40 rst_n <= 1;
+    wdata <= 22'b0000000001000000000001;
+    wen <= 1;
+    #20
+    wen <= 0; 
+    patch <= 55'b000000000011000000000011000000000011000000000001000000000011; //This will give use left if indexed properly
+    valid <=1;
+    #20
+    assert(valid_left == 1'b1)
+    assert(valid_right == 1'b0)
+    patch <= 55'b000000000011000000000011000000000011000000000011000000000011; //This will give us right
+    #20
+    assert(valid_left == 1'b0)
+    assert(valid_right == 1'b1)
+    valid <=0;
+    wdata <= 22'b0000000001000000000100;
+    wen <= 1;
+    #20
+    wen <=0;
+    patch <= 55'b000000000000000000000011000000000011000000000001000000000011; //This will give use left if indexed properly
+    valid <=1;
+    #20
+    assert(valid_left == 1'b1)
+    assert(valid_right == 1'b0)
+
+
+
+   end
+
+
+
+    initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+//     $vcdplusmemon();
+//     $vcdpluson(0, aggregator_tb);
+    #2000;
+    $finish(2);
+  end

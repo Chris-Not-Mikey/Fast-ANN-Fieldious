3675e1c6915fde69fcea482e5c46ef2bac8afc68
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-10 21:16:58 -0700

    Update internal_node_tree.v

--- rtl/internal_node_tree.v
+++ rtl/internal_node_tree.v
@@ -75,13 +75,15 @@ end
 
 
 reg [PATCH_WIDTH-1:0] level_patches [7:0]; //For storing patch
-wire level_valid [255:0][7:0]; //for storing valid signals
+reg level_valid [255:0][7:0]; //for storing valid signals
+wire level_valid_storage [255:0][7:0]; //for storing valid signals
 reg empty_valid [255:0];
 
-assign level_valid[0][0] = 255'b1;
+//assign 
 
 always @(*) begin
     
+    level_valid[0][0] = 255'b1;
     level_patches[0] = patch_in;
  
      //Fill empties
@@ -126,8 +128,8 @@ generate
             .wdata(sender_data), //writing mechanics are NOT pipelined
             .patch_in(level_patches[i]),
             .patch_out(patch_out), //TODO REMOVE this, we don't need to store this at the internal node level
-            .valid_left(level_valid[j*2][i]),
-            .valid_right(level_valid[(j*2)+1][i])
+            .valid_left(level_valid_storage[j*2][i]),
+            .valid_right(level_valid_storage[(j*2)+1][i])
             );
 
         //  assign valid_output[(j*2)+1:(j*2)] = vl;
@@ -147,17 +149,17 @@ generate
             if (rst_n == 0) begin
                 level_patches[i+1] <= 0;
                 //level_valid[i+1] <= empty_valid;
-//                  for (int r = 0; r < 255; r++) begin
-//                      level_valid[r][i+1] = 1'b0;
-//                  end
+                 for (int r = 0; r < 255; r++) begin
+                     level_valid[r][i+1] = 1'b0;
+                 end
              
             end
             else begin
                 level_patches[i+1] <= level_patches[i];
                 //level_valid[i+1] <= level_valid[i];
-//                  for (int r = 0; r < 255; r++) begin
-//                     level_valid[r][i+1] = level_valid[r][i];
-//                  end
+                 for (int r = 0; r < 255; r++) begin
+                    level_valid[r][i+1] = level_valid_storage[r][i];
+                 end
             end
 
         end

fb07a64221afbe7702b253e57b4800f89cbec32e
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-10 21:07:18 -0700

    Update internal_node_tree.v

--- rtl/internal_node_tree.v
+++ rtl/internal_node_tree.v
@@ -73,7 +73,7 @@ end
 
 
 reg [PATCH_WIDTH-1:0] level_patches [7:0]; //For storing patch
-reg level_valid [255:0][7:0]; //for storing valid signals
+wire level_valid [255:0][7:0]; //for storing valid signals
 reg empty_valid [255:0];
 
 
@@ -144,17 +144,17 @@ generate
             if (rst_n == 0) begin
                 level_patches[i+1] <= 0;
                 //level_valid[i+1] <= empty_valid;
-                 for (int r = 0; r < 255; r++) begin
-                    level_valid[r][i+1] = 1'b0;
-                 end
+//                  for (int r = 0; r < 255; r++) begin
+//                      level_valid[r][i+1] = 1'b0;
+//                  end
              
             end
             else begin
                 level_patches[i+1] <= level_patches[i];
                 //level_valid[i+1] <= level_valid[i];
-                 for (int r = 0; r < 255; r++) begin
-                    level_valid[r][i+1] = level_valid[r][i];
-                 end
+//                  for (int r = 0; r < 255; r++) begin
+//                     level_valid[r][i+1] = level_valid[r][i];
+//                  end
             end
 
         end

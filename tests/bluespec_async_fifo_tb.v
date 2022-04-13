5b3e1f361fa9978dd7746d709d6c0a41e8d9fc43
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-12 22:57:46 -0700

    Update bluespec_async_fifo_tb.v

--- tests/bluespec_async_fifo_tb.v
+++ tests/bluespec_async_fifo_tb.v
@@ -53,7 +53,7 @@ module bluespec_syncfifo_tb;
 
     for (int iter=0; iter<2; iter++) begin
       for (int i=0; i<32; i++) begin
-        @(posedge wclk iff !wfull);
+        @(posedge wclk iff wfull);
         winc = (i%2 == 0)? 1'b1 : 1'b0;
         if (winc) begin
           wdata = $urandom;
@@ -73,7 +73,7 @@ module bluespec_syncfifo_tb;
 
     for (int iter=0; iter<2; iter++) begin
       for (int i=0; i<32; i++) begin
-        @(posedge rclk iff !rempty)
+        @(posedge rclk iff rempty)
         rinc = (i%2 == 0)? 1'b1 : 1'b0;
         if (rinc) begin
           verif_wdata = verif_data_q.pop_back();

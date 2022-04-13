11e066f8a93d65cb4e59a660a6b20c268ec269ec
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-13 00:24:49 -0700

    Update aggregator_tb.v

--- tests/aggregator_tb.v
+++ tests/aggregator_tb.v
@@ -93,7 +93,7 @@ module aggregator_tb;
     #20
     //iseven = 1'b0; 
     change_fetch_width = 1'b1;
-    local_fetch_width = 3'd4;
+    local_fetch_width = 3'd6;
 
 
     
@@ -154,7 +154,7 @@ module aggregator_tb;
 
   genvar i;
   generate
-   for (i = 0; i < 4; i++) begin
+	  for (i = 0; i < 6; i++) begin
       always @ (posedge clk) begin
         if (receiver_enq  ) begin
           assert(receiver_din[(i + 1)*`DATA_WIDTH - 1 : i * `DATA_WIDTH] == expected_dout + i);

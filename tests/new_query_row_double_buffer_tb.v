f2649219323f52f686e047d2e45f0e8f7126da89
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-14 11:41:37 -0700

    Update new_query_row_double_buffer_tb.v

--- tests/new_query_row_double_buffer_tb.v
+++ tests/new_query_row_double_buffer_tb.v
@@ -269,6 +269,10 @@ end
 	$display("%t: received = %d, expected = %d", $time, rpatch0, hold_expected);
         $display("%t: received = %d, expected = %d", $time, rpatch0[10:0], hold_expected[10:0]);
         $display("%t: received = %d, expected = %d", $time, rpatch0[21:11], hold_expected[21:11]);
+	$display("%t: received = %d, expected = %d", $time, rpatch0[32:22], hold_expected[32:22]);
+        $display("%t: received = %d, expected = %d", $time, rpatch0[43:33], hold_expected[43:33]);
+	$display("%t: received = %d, expected = %d", $time, rpatch0[54:44], hold_expected[54:44]);
+
 		    
 	    end
 		

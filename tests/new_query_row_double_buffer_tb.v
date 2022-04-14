b87ec20c3fe8dfb22b61f5b9fe8f25a1afc90cfc
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-14 16:21:22 -0700

    Update new_query_row_double_buffer_tb.v

--- tests/new_query_row_double_buffer_tb.v
+++ tests/new_query_row_double_buffer_tb.v
@@ -211,7 +211,7 @@ end
    
 	  
 	  
-    #7000
+    #17000
     addr0 <= 0;
     write_disable <= 1;
     #100
@@ -338,7 +338,7 @@ end
     $dumpvars;
 //     $vcdplusmemon();
 //     $vcdpluson(0, aggregator_tb);
-    #10400;
+    #20400;
     $finish(2);
   end
 

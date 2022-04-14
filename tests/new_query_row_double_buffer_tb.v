e26e55612b0dc5651daa71a0d2420e6aef3a6610
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-14 11:52:51 -0700

    Update new_query_row_double_buffer_tb.v

--- tests/new_query_row_double_buffer_tb.v
+++ tests/new_query_row_double_buffer_tb.v
@@ -265,7 +265,7 @@ end
         addr0 <= addr0 + 1;
         addr1 <= addr1 + 1;
 
-        assert(rpatch1 == hold_expected);
+	assert(rpatch0 == hold_expected);
 	$display("%t: received = %d, expected = %d", $time, rpatch0, hold_expected);
         $display("%t: received = %d, expected = %d", $time, rpatch0[10:0], hold_expected[10:0]);
         $display("%t: received = %d, expected = %d", $time, rpatch0[21:11], hold_expected[21:11]);

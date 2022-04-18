55ff89bddbb1766ffb1f65c3efb1352cdb5924aa
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-17 22:05:14 -0700

    Update full_test_tb.v

--- tests/full_test_tb.v
+++ tests/full_test_tb.v
@@ -752,9 +752,9 @@ end
 
             	//csb0 <= 0; //active low
 		     assert(rleaf0[0] == hold_leaf_expected[47:0]);
-		     $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[0], hold_leaf_expected[47:0]);
-// 		    $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[10:0], hold_leaf_expected[10:0]);
-// 		    $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[21:11], hold_leaf_expected[21:11]);
+		     $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[0][10:0], hold_leaf_expected[10:0]);
+		     $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[0][21:11], hold_leaf_expected[21:11]);
+		     $display("%t: (LEAF) received = %d, expected = %d", $time, rleaf0[0][32:22], hold_leaf_expected[32:22]);
 		    // $display("%t: received = %d, expected = %d", $time, rpatch0[32:22], hold_expected[32:22]);
 		    // $display("%t: received = %d, expected = %d", $time, rpatch0[43:33], hold_expected[43:33]);
 		    // $display("%t: received = %d, expected = %d", $time, rpatch0[54:44], hold_expected[54:44]);

21316c723b3efa69c424d95bf3b4d02933a1e810
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-19 00:03:14 -0700

    Update construct.py

--- skywater-digital-flow/InternalNodeTree/design/construct.py
+++ skywater-digital-flow/InternalNodeTree/design/construct.py
@@ -27,7 +27,7 @@ def construct():
 
   parameters = {
     'construct_path' : __file__,
-    'design_name'    : 'internal_node_tree',
+    'design_name'    : 'internal_node',
     'clock_period'   : 6.66666667,
     'adk'            : adk_name,
     'adk_view'       : adk_view,

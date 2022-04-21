f4d8f28ff984d1a958f4f26185a298c3f969ada2
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-20 22:06:36 -0700

    Update internal_node.v

--- rtl/internal_node.v
+++ rtl/internal_node.v
@@ -72,11 +72,11 @@ end
  //NOTE: some testbenches have this order flipped (think endianess) You may need to flip the order of these case statements
 always @(*) begin 
     case(idx)
-       3'b000 :   sliced_patch = patch_in[10:0];
-       3'b001 :   sliced_patch = patch_in[21:11];
+       3'b100 :   sliced_patch = patch_in[10:0];
+       3'b011 :   sliced_patch = patch_in[21:11];
        3'b010 :   sliced_patch = patch_in[32:22];
-       3'b011 :   sliced_patch = patch_in[43:33];
-       3'b100 :   sliced_patch = patch_in[54:44];
+       3'b001 :   sliced_patch = patch_in[43:33];
+       3'b000 :   sliced_patch = patch_in[54:44];
        default :  sliced_patch = 11'b0;
     endcase 
 end

7f8bcd1a9841dee5be4fbc0d99562b2c2ea606d1
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-04-29 16:31:56 -0700

    Update query_row_double_buffer_tb.v

--- tests/query_row_double_buffer_tb.v
+++ tests/query_row_double_buffer_tb.v
@@ -516,7 +516,7 @@ end
      
       write_latency_counter <= 0;
 	  
-	  if (patch_counter == 9'd442) begin
+	  if (patch_counter == 9'd494) begin
 		  
 		  //Stop writing, start reading (TOP LEVEL: Include counter register like this)
 		write_disable <= 1;

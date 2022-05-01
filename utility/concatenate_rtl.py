29fd2a96ef5f1e524129669bc75fd7845ad9b7a6
Chris <christophercalloway216@gmail.com>
2022-04-30 22:15:52 -0700

    added file to concatenate rtl for synthesis

---
+++ utility/concatenate_rtl.py
@@ -0,0 +1,53 @@
+import os
+
+
+################################################################
+#                   concatenate_rtl.PY                         #
+################################################################
+# Concatenates all relevant rtl code together for synthesis    # 
+#                                                              #
+# Author: Chris Calloway, cmc2734@stanford.edu                 #
+# Maintainers: Chris Calloway, cmc2734@stanford.edu            #
+#           Jake Ke, jakeke@stanford.edu                       #
+################################################################
+
+# Files that WILL NOT be synthesized 
+# Anything else in rtl direcotrt will be synthesized
+#skip_files = ["dual_clock_async_fifo_design.v", "internal_node.v", "fifo.v", "deaggregator.v", "async_fifo.v", "rtl_all.v", "QueryPatchMem_2.v",  ]
+
+
+#Take from runtests.py for top level
+allow_files = ['top.sv', 'QueryPatchMem.v',  'aggregator.v', 'sram_1kbyte_1rw1r.v', 'sky130_sram_1kbyte_1rw1r_32x256_8.v', 'internal_node_2.v', 'internal_node_tree_2.v', 'LeavesMem.sv', 'BitonicSorter.sv', 'kBestArrays.sv', 'L2Kernel.sv', 'MainFSM.sv', 'RunningMin.sv', 'SyncFIFO.v', 'SortedList.sv']
+
+
+if __name__ == "__main__":
+
+    directory = "../rtl"
+
+    f_rtl_str = "rtl_all.v"
+
+    f_rtl = open(f_rtl_str, "w")
+
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        
+
+        if os.path.isfile(f) and filename in allow_files:
+            print(f)
+
+            with open(f, "r") as copy_file:
+
+                for line in copy_file:
+                    f_rtl.write(line)
+
+                #Add a newline at the end incase it wasn't already there
+                f_rtl.write("\n")
+                f_rtl.write("\n")
+
+
+            copy_file.close()
+
+
+
+    f_rtl.close()

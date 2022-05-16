700009cffb27217a4c9ab07b9cb9c56ad453f4fd
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-05-15 21:23:43 -0700

    Update WishBoneCtrl_tb.sv

--- tests/WishBoneCtrl_tb.sv
+++ tests/WishBoneCtrl_tb.sv
@@ -368,13 +368,13 @@ module WishBoneCtrl_tb();
         wbs_we_i = 1'b1;
         wbs_sel_i = '1;
         wbs_dat_i = {10'b0, 11'd55, 11'd1}; //10 0's, median of 55, and index of 1 
-        wbs_adr_i = WBS_NODE_ADDR + 1'b1  + 0; // addr 1
+        wbs_adr_i = WBS_NODE_ADDR + 2'b2  + 0; // addr 1
     
         @(negedge wbs_ack_o);
         wbs_cyc_i = 1'b1;
         wbs_stb_i = 1'b1;
         wbs_we_i = 1'b0;
-        wbs_dat_i = '0l
+        wbs_dat_i = '0;
  
 
         @(negedge wbs_ack_o);

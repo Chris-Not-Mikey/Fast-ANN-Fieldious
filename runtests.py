36b28a4e4004af61dc9b0f5629dbb31431735263
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-05-22 14:43:03 -0700

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -248,8 +248,10 @@ def test_top_wrapper_multi_tb():
     run_python_process(['python3', './gold/gold.py', args.images[0], args.images[1], './data/IO_data/', 'multi'])
     run_process(['vcs', '-full64', '-sverilog', '-timescale=1ns/1ps', '-debug_access+pp', 'tests/top_wrapper_multi_tb.sv', 'rtl/user_proj_example.v', 'rtl/SyncPulse.v', 'rtl/SyncBit.v', 'rtl/ClockMux.v', 'rtl/ResetMux.v', 'rtl/WishBoneCtrl.sv',  'rtl/top.sv', 'rtl/QueryPatchMem.v',  'rtl/aggregator.v', 'rtl/sram_1kbyte_1rw1r.v', 'rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v', 'rtl/internal_node_2.v', 'rtl/internal_node_tree_2.v', 'rtl/LeavesMem.sv', 'rtl/BitonicSorter.sv', 'rtl/kBestArrays.sv', 'rtl/L2Kernel.sv', 'rtl/MainFSM.sv', 'rtl/RunningMin.sv', 'rtl/SyncFIFO.v', 'rtl/SortedList.sv'])
     run_python_process(['python3', './gold/l2.py', args.images[0], args.images[1], "0"])
+    run_python_process(['python3', './gold/l2.py', "vid_1", "vid_2", "1"])
+    #final arg (for example 1, or 4) means which image pair in the list that it is (see goldmodel for list)
     
-    return 1, run_python_process(['python3', './gold/l2.py', "vid_1", "vid_2", "1"])
+    return 1, run_python_process(['python3', './gold/l2.py', "vid_7", "vid_8", "4"])
 
 
 def test_top_wrapper_tb():
